26ae4b92407ee5aaba357f8b8fa786d30e87b839
shabeer_p_p <shabeer@zanyautomation.com>
2024-12-12 10:09:58 +0530

    added

--- backend/chat/routing.py
+++ backend/chat/routing.py
@@ -3,7 +3,7 @@ from django.urls import re_path
 from . import consumers 
 
 websocket_urlpatterns=[
-    re_path(r'chat/(?P<sender_id>\w+)/$', consumers.PersonalChatConsumer.as_asgi()),
-    re_path(r'ws/(?P<room_name>\w+)/$',consumers.ChatConsumer.as_asgi()),
-    re_path(r'online/$',consumers.OnlineConsumer.as_asgi()),
+    re_path(r'api/chat/(?P<sender_id>\w+)/$', consumers.PersonalChatConsumer.as_asgi()),
+    re_path(r'api/ws/(?P<room_name>\w+)/$',consumers.ChatConsumer.as_asgi()),
+    re_path(r'api/online/$',consumers.OnlineConsumer.as_asgi()),
 ]
