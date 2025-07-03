2b47bac31019d50c0ef0510b871e18b82b7c7680
shabeertsy <shabeerpptsy@gmail.com>
2025-07-03 18:16:31 +0530

    cma app added

--- backend/lms/settings.py
+++ backend/lms/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'chat',
     'rest_framework_simplejwt',
     'corsheaders',
+    'cma',
 
 ]
 
@@ -66,7 +67,7 @@ ROOT_URLCONF = 'lms.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
